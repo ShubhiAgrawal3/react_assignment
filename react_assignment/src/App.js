4e0099c1adedc18518219b8a4405a9a6b6281f63
gem-ShubhiAgrawal <Shubhi.Agrawal@Geminisolutions.com>
2022-05-24 12:57:10 +0530

    login commit

--- react_assignment/src/App.js
+++ react_assignment/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Registration from './components/Registration';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import LogIn from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Registration></Registration>
+      <div className='outer'>
+        <div className='inner'>
+        <Registration></Registration>
+    
+        </div>
+      </div>
+      
     </div>
   );
 }
-
+ 
 export default App;
